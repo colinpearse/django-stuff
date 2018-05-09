adde19064c99aa39ce038c52f489e9fd03596d7f
cassmac <colinpearse@cassmac-2.local>
2018-05-09 09:56:43 -0500

    Heroku update

--- coleg2/coleg2/settings.py
+++ coleg2/coleg2/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a(%7x1xc_wvm7xu&v32(p@+k!6^%vc!ci@$g745p0i0#+famy*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
 # For Pythonanywhere
 #ALLOWED_HOSTS = ['192.168.0.5', '.localdomain', 'colinpearse.pythonanywhere.com']
